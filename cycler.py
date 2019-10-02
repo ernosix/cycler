031417515b557c13c493a55a0a7f1769a9178327
ernosix <shane@erno.ca>
2019-10-01 21:32:27 -0700

    added descriptive statuses below time, finalizing

--- cycler.py
+++ cycler.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 import pifacedigitalio
 
 mode = 'HEAT'
-countdown = 8
+minutes = 7
+seconds = 47
 
 
 def quit(*args):
@@ -23,22 +24,27 @@ def show_time():
     # If below 3:45 set text color yellow
     if int(remainder.total_seconds()) < 225:
         lbl.configure(foreground="yellow")
+        status.configure(foreground="yellow")
+        status_txt.set("CHANGING MODE")
 
     # At 0:00 cycle modes
     if int(remainder.total_seconds()) < 1:
         if mode == 'HEAT':
             mode = 'COOL'
             lbl.configure(foreground="blue")
+            status.configure(foreground="blue")
             pifacedigital.relays[0].turn_off()
         else:
             mode = 'HEAT'
             lbl.configure(foreground="red")
+            status.configure(foreground="red")
             pifacedigital.relays[0].turn_on()
-        endTime = datetime.now() + timedelta(minutes=countdown)
+        endTime = datetime.now() + timedelta(minutes=minutes, seconds=seconds)
 
     # Show the time left
     remainder = remainder - timedelta(microseconds=remainder.microseconds)
     txt.set(remainder)
+    status_txt.set(mode + "ING MODE")
 
     # Loop
     root.after(1000, show_time)
@@ -55,12 +61,15 @@ root.configure(background='black')
 root.bind("x", quit)
 root.after(1000, show_time)
 
-# Set the end date and time for the countdown loop (8 min)
-endTime = datetime.now() + timedelta(minutes=countdown)
+# Set the end date and time for the countdown loop
+endTime = datetime.now() + timedelta(minutes=minutes, seconds=seconds)
 
 fnt = font.Font(family='Helvetica', size=280, weight='bold')
+status_font = font.Font(family='Helvetica', size=120, weight='bold')
 txt = StringVar()
+status_txt = StringVar()
 lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="red", background="black")
-lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
-
+lbl.place(relx=0.5, rely=0.4, anchor=CENTER)
+status = ttk.Label(root, textvariable=status_txt, font=status_font, foreground="red", background="black")
+status.place(relx=0.5, rely=0.8, anchor=CENTER)
 root.mainloop()
