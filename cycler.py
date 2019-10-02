1dcc187b4c87088f5947f904a803b6ea95a0fe43
ernosix <shane@erno.ca>
2019-10-02 08:55:43 -0700

    removed hour field

--- cycler.py
+++ cycler.py
@@ -2,16 +2,15 @@
 
 from tkinter import *
 from tkinter import ttk, font
+#import pifacedigitalio
 from datetime import datetime, timedelta
-import pifacedigitalio
 
-mode = 'HEAT'
 minutes = 7
 seconds = 47
 
 
 def quit(*args):
-    pifacedigital.relays[0].turn_off()
+#    pifacedigital.relays[0].turn_off()
     root.destroy()
 
 
@@ -35,16 +34,18 @@ def show_time():
         if mode == 'HEAT':
             mode = 'COOL'
             foreground_color = "blue"
-            pifacedigital.relays[0].turn_off()
+#            pifacedigital.relays[0].turn_off()
         else:
             mode = 'HEAT'
             foreground_color = "red"
-            pifacedigital.relays[0].turn_on()
+#            pifacedigital.relays[0].turn_on()
         endTime = datetime.now() + timedelta(minutes=minutes, seconds=seconds)
 
     # Show the time left
     remainder = remainder - timedelta(microseconds=remainder.microseconds)
-    txt.set(remainder)
+    time_left = str(remainder)
+    time_left = time_left.replace(time_left[:2], '')
+    txt.set(time_left)
     status.configure(foreground=foreground_color)
     lbl.configure(foreground=foreground_color)
 
@@ -53,8 +54,8 @@ def show_time():
 
 
 # Initialize PiFace, turn on relay 1
-pifacedigital = pifacedigitalio.PiFaceDigital()
-pifacedigital.relays[0].turn_on()
+#pifacedigital = pifacedigitalio.PiFaceDigital()
+#pifacedigital.relays[0].turn_on()
 
 # Use tkinter lib for showing the clock
 root = Tk()
@@ -65,9 +66,9 @@ root.after(1000, show_time)
 
 # Set the end date and time for the countdown loop
 endTime = datetime.now() + timedelta(minutes=minutes, seconds=seconds)
-
+mode = 'HEAT'
 foreground_color = "red"
-fnt = font.Font(family='Helvetica', size=280, weight='bold')
+fnt = font.Font(family='Helvetica', size=380, weight='bold')
 status_font = font.Font(family='Helvetica', size=120, weight='bold')
 txt = StringVar()
 status_txt = StringVar()
